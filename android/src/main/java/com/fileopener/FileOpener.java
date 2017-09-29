8c32b13100f562a6269caf40dc5fc284e220878a
Jean de Campredon <jean.decampredon@gmail.com>
2017-09-29 14:18:32 +0200

    chore: change android loader

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -1,138 +1,59 @@
 package com.fileopener;
 
-import java.io.File;
-
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.content.pm.ResolveInfo;
-import android.content.ComponentName;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Build;
-import android.support.v4.content.FileProvider;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.util.Map;
+import org.json.JSONException;
+
+import java.io.File;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class FileOpener extends ReactContextBaseJavaModule {
-  private Context context;
 
-  public FileOpener(ReactApplicationContext reactContext) {
-    super(reactContext);
-    context = reactContext;
-  }
+    public FileOpener(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-    return "FileOpener";
-  }
+    @Override
+    public String getName() {
+        return "FileOpener";
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    return constants;
-  }
-
-  @ReactMethod
-  public void open(String fileArg, String contentType, Promise promise) throws JSONException {
-  		File file = new File(fileArg);
-
-  		if (file.exists()) {  			try {
-  				Uri path = Uri.fromFile(file);
-  				Intent intent = new Intent(Intent.ACTION_VIEW);
-                if (Build.VERSION.SDK_INT >= 24) {
-                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    Uri contentUri = FileProvider.getUriForFile(getReactApplicationContext(), getReactApplicationContext().getPackageName() + ".fileprovider", file);
-                    intent.setDataAndType(contentUri, contentType);
-                } else {
-                    intent.setDataAndType(path, contentType);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                }
-
-  				getReactApplicationContext().startActivity(intent);
-
-          promise.resolve("Open success!!");
-  			} catch (android.content.ActivityNotFoundException e) {
-          promise.reject("Open error!!");
-  			}
-  		} else {
-          promise.reject("File not found");
-  		}
-  	}
-
-    @ReactMethod
-    public void openApp(String packagename, Promise promise) throws JSONException {
-       PackageInfo packageinfo = null;  
-        try {
-          packageinfo = context.getPackageManager().getPackageInfo(packagename, 0);
-
-          if (packageinfo == null) {
-            promise.reject("Open error!!");
-          } else {
-            // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent 
-            Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);  
-            resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);  
-            resolveIntent.setPackage(packageinfo.packageName);  
-
-            // 通过getPackageManager()的queryIntentActivities方法遍历 
-            List<ResolveInfo> resolveinfoList = context.getPackageManager()  
-                    .queryIntentActivities(resolveIntent, 0);  
-
-            ResolveInfo resolveinfo = resolveinfoList.iterator().next();  
-            if (resolveinfo != null) {  
-                // packagename = 参数packname 
-                String packageName = resolveinfo.activityInfo.packageName;  
-                // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname] 
-                String className = resolveinfo.activityInfo.name;  
-                // LAUNCHER Intent 
-                Intent intent = new Intent(Intent.ACTION_MAIN);  
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);  
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                // 设置ComponentName参数1:packagename参数2:MainActivity路径 
-                ComponentName cn = new ComponentName(packageName, className);  
-
-                intent.setComponent(cn);  
-                getReactApplicationContext().startActivity(intent);  
-            }
-            promise.resolve("Open success!!"); 
-          }          
-        } catch (NameNotFoundException e) {  
-          promise.reject("Open error!!");
-          // e.printStackTrace();
-        }  
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        return constants;
     }
 
     @ReactMethod
-    public void isInstalledApp(String packagename, Promise promise) throws JSONException {
-       PackageInfo packageinfo = null;  
+    public void open(String fileArg, String contentType, Promise promise) throws JSONException {
+        Uri path = null;
+        if (fileArg.startsWith("content:")) {
+            path = Uri.parse(fileArg);
+        } else {
+            File file = new File(fileArg);
+            if (file.exists()) {
+                path = Uri.fromFile(file);
+            } else {
+                promise.reject("FileOpener.java: File not found");
+                return;
+            }
+        }
         try {
-          packageinfo = context.getPackageManager().getPackageInfo(packagename, 0);
-          if (packageinfo == null) {
-            promise.reject("Open error!!");
-          } else {            
-            promise.resolve("Open success!!"); 
-          }          
-        } catch (NameNotFoundException e) {  
-          promise.reject("Open error!!");          
-        }  
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(path, contentType);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getReactApplicationContext().startActivity(intent);
+            promise.resolve("FileOpener.java: open success");
+        } catch (android.content.ActivityNotFoundException e) {
+            promise.reject("FileOpener.java: open error - activity not found");
+        }
     }
 }
